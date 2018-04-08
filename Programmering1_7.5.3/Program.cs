fa394dbc5f8229134cedf755ee0ea5d81ebe906c
Monica Björk <m.bjork95@gmail.com>
2018-04-08 09:23:25 +0200

    Add project files.

---
+++ Programmering1_7.5.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_7._5._3 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Hur många gånger vill du singla slant? ");
+            int singlaSlant = Convert.ToInt32(Console.ReadLine());
+            Random rnd = new Random();  // Must be outside the loop to work.
+            
+            while(singlaSlant>0) {
+                int kronaKlave = rnd.Next(2);
+                singlaSlant--;  // Correct number of repeats since it counts down. 
+
+                if (kronaKlave==0) {
+                    Console.WriteLine("Det blev krona!");
+                }
+                else {
+                    Console.WriteLine("Det blev klave!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
